d6a1133ae4f15438c7fbfe3069463735f8ed8f15
Nimesh Silva <nimeshsilva@gmail.com>
2020-03-21 18:31:03 -0400

    Next Commit.

--- react_express_starter/reactexpress/client/src/components/customers/customers.js
+++ react_express_starter/reactexpress/client/src/components/customers/customers.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import './customers.css';
 
 class Customers extends Component{
+    constructor() {
+        super();
+        this.state = {
+            customers: [] //Empty customers array in state
+        }
+    }
+//Runs automatically when the component is run
+    componentDidMount() {
+        fetch("/api/customers") //Call to our backend
+            .then(res => res.json())
+            .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers))); //Setting our state
+    }
+
     render() {
         return (
             <div>
