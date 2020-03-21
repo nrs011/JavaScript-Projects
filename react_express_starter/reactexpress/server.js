22b72185140da876f3077a5df3049fcc8ade68b5
Nimesh Silva <nimeshsilva@gmail.com>
2020-03-21 18:35:34 -0400

    Next Commit.

--- react_express_starter/reactexpress/server.js
+++ react_express_starter/reactexpress/server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/api/customers', (req, res) => {
     const customers = [
-        {id: 1, fistName: 'John', lastName: 'Doe'},
-        {id: 2, fistName: 'Steve', lastName: 'Smith'},
-        {id: 3, fistName: 'Mary', lastName: 'Swanson'},
+        {id: 1, firstName: 'John', lastName: 'Doe'},
+        {id: 2, firstName: 'Steve', lastName: 'Smith'},
+        {id: 3, firstName: 'Mary', lastName: 'Swanson'},
 
     ];
     res.json(customers);
