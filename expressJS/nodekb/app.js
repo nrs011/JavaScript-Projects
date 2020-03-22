50217bcb4fa5e61eeb0002f32c95274e9595b805
Nimesh Silva <nimeshsilva@gmail.com>
2020-03-22 15:36:37 -0400

    Next Commit.

--- expressJS/nodekb/app.js
+++ expressJS/nodekb/app.js
@@ -1,15 +1,13 @@
 const express = require('express'); //bringing in a variable called express
 const path = require('path');
 const mongoose = require('mongoose');
-const  bodyParser = require('body-parser');
 //Connect to database
-mongoose.set('useUnifedTopology', true);
-mongoose.connect('mongodb://localhost/nodekb', {useNewUrlParser:true});
+mongoose.connect('mongodb://localhost/nodekb', {useNewUrlParser:true, useUnifiedTopology: true});
 let db = mongoose.connection;
 
 //Check connection
-db.once('opne', function () {
-    console.log('Connected to MongoDB')
+db.once('open', function () {
+    console.log('Connected to MongoDB.')
 });
 
 //Check for DB errors
@@ -20,13 +18,26 @@ db.on('error', function () {
 //Init App
 const app = express(); //calling it here
 
+//Bring in Models
+let Article = require('./models/article');
+
 //Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 //Home Route
 app.get('/', function (req, res) {
-    let articles = [
+    Article.find({}, function(err, articles) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
+        }
+    });
+    /*let articles = [
         {
             id: 1,
             title: 'Article One',
@@ -45,11 +56,7 @@ app.get('/', function (req, res) {
             author: 'Nimesh Silva',
             body: 'This is article three'
         },
-    ];
-    res.render('index', {
-        title: 'Articles',
-        articles: articles
-    });
+    ];*/
 });
 
 //Add Route
