c227f753d17c8b78784ca0441832c41c4077c8d3
Nimesh Silva <nimeshsilva@gmail.com>
2020-03-22 19:58:09 -0400

    Next Commit.

--- expressJS/nodekb/app.js
+++ expressJS/nodekb/app.js
@@ -2,6 +2,10 @@ const express = require('express'); //bringing in a variable called express
 const path = require('path');
 const mongoose = require('mongoose');
 const  bodyParser = require('body-parser');
+//const { check, validationResult } = require('express-validator/check');
+const  expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 //Connect to database
 mongoose.connect('mongodb://localhost/nodekb', {useNewUrlParser:true, useUnifiedTopology: true});
 let db = mongoose.connection;
@@ -36,6 +40,37 @@ app.use(bodyParser.json());
 //Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true}
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
 //Home Route
 app.get('/', function (req, res) {
     Article.find({}, function(err, articles) {
