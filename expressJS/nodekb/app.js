6ec79999ff6761fb079148b09565c55743e2d3fe
Nimesh Silva <nimeshsilva@gmail.com>
2020-03-21 23:11:11 -0400

    Next Commit.

--- expressJS/nodekb/app.js
+++ expressJS/nodekb/app.js
@@ -1,11 +1,19 @@
 const express = require('express'); //bringing in a variable called express
+const path = require('path');
 
+//Init App
 const app = express(); //calling it here
-//routes
+
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Home Route
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
+//Start Server
 app.listen(3000, function () {
     console.log('Server started on port 3000.')
 });
